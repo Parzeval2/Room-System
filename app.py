41a23ba7755d77e527c60ef07f7798c433de0ce4
Grant Cooper <69401337+Parzeval2@users.noreply.github.com>
2023-03-28 10:49:04 -0500

    Virtual Environment for consistency and also added debug to app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
-
+#setup for flask app
 app = Flask(__name__)
+
+############################
+#Debug
+app.debug = False
+############################
+
 
 @app.route("/")
 def hello_world():
