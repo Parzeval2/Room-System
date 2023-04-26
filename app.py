b4b126de8c7863599fb2f970d919e1c6320e2a30
Restyled.io <commits@restyled.io>
2023-04-26 14:51:08 +0000

    Restyled by black

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ import Group_Class
 import Queue
 from Room_Class import Room
 from timeloop import Timeloop
+
 # setup for flask app
 app = Flask(__name__)
 
-ROOMS = {'111': Room('111'), '125': Room('125'), '131': Room('131'),
-         '211': Room('211'), '225': Room('225'), '231': Room('231'),
-         '311': Room('311'), '325': Room('325'), '331': Room('331')}
+ROOMS = {
+    "111": Room("111"),
+    "125": Room("125"),
+    "131": Room("131"),
+    "211": Room("211"),
+    "225": Room("225"),
+    "231": Room("231"),
+    "311": Room("311"),
+    "325": Room("325"),
+    "331": Room("331"),
+}
 tl = Timeloop()
 
 
@@ -34,25 +43,25 @@ def hello_world():
     return render_template("index.html", title="Hello")
 
 
-@app.route('/register_group')
+@app.route("/register_group")
 def register_group():
-    return render_template('register_group.html')
+    return render_template("register_group.html")
 
 
-@app.route('/join_queue')
+@app.route("/join_queue")
 def join_queue(group):
     queue.join_queue(group)
     flash("You've joined the queue")
-    return redirect(url_for('queue'))
+    return redirect(url_for("queue"))
 
 
-@app.route('/leave_queue')
+@app.route("/leave_queue")
 def leave_queue(group):
     queue.leave_queue(group)
     flash("You've left the queue")
-    return redirect(url_for('queue'))
+    return redirect(url_for("queue"))
 
 
-@app.route('/queue')
+@app.route("/queue")
 def queue():
-    return render_template('queue.html', queue=queue)
+    return render_template("queue.html", queue=queue)
